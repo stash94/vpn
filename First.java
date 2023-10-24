872006843a22e2c99cc4ff38cd73ee5b99d41df9
satyasws <satya.prakash@swspvtltd.com>
2023-10-24 22:42:50 +0530

    added easy level progms

---
+++ First.java
@@ -0,0 +1,12 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Enter two numbers");
+    int first = 10;
+    int second = 20;
+    System.out.println(first + " " + second);
+    // add two numbers
+    System.out.println("The sum is: " + first + second);
+  }
+}
